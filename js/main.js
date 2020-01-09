195567c47871fdc241be28c6128c0b9cfdbddf26
Sul3iman Mutabazi <47069109+Muchstix@users.noreply.github.com>
2020-01-09 20:37:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $('.center').slick(
   
     {
     infinite: true,
-    autoplay: true,
+    autoplay: false,
     speed: 300,
     centerMode: true,
     dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
     centerPadding: '60px',
     slidesToShow: 3,
     responsive: [
@@ -25,9 +27,34 @@ $('.center').slick(
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 1
+          slidesToShow: 1,
         }
       }
     ]
   }
-  );
+);
+
+$('.single-item').slick({
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    centerMode: true,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+    centerPadding: '60px',
+});
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    nfinite: true,
+    autoplay: false,
+    speed: 300,
+    centerMode: true,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+    centerPadding: '60px',
+    slidesToScroll: 3
+});
