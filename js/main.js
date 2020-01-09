6d004aa7aa4b3975fd2c3dc74a0fe6ba35be9ab7
Sul3iman Mutabazi <47069109+Muchstix@users.noreply.github.com>
2020-01-09 17:05:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/* First Slider */
+$('.center').slick(
+  
+    {
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    centerMode: true,
+    dots: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  }
+  );
